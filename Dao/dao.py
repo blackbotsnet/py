9f02aee50046741d681078c176de246b0f4377ce
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2023-07-03 19:27:13 -0400

    Update dao.py

--- Dao/dao.py
+++ Dao/dao.py
@@ -29,40 +29,7 @@ import threading
 import sys
 import time
 from bs4 import BeautifulSoup
-from contextlib import contextmanager
 
-@contextmanager
-def st_redirect(src, dst):
-    placeholder = st.empty()
-    output_func = getattr(placeholder, dst)
-
-    with StringIO() as buffer:
-        old_write = src.write
-
-        def new_write(b):
-            if getattr(current_thread(), SCRIPT_RUN_CONTEXT_ATTR_NAME, None):
-                buffer.write(b)
-                output_func(buffer.getvalue())
-            else:
-                old_write(b)
-
-        try:
-            src.write = new_write
-            yield
-        finally:
-            src.write = old_write
-
-
-@contextmanager
-def st_stdout(dst):
-    with st_redirect(sys.stdout, dst):
-        yield
-
-
-@contextmanager
-def st_stderr(dst):
-    with st_redirect(sys.stderr, dst):
-        yield
 
 st.header("BlackDao")
 res_box=st.empty()
