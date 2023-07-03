13b35194a8b84b21c7fc784e5cff32778bbec94c
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2023-07-03 19:46:10 -0400

    Update dao.py

--- Dao/dao.py
+++ Dao/dao.py
@@ -10,9 +10,7 @@ try:
     from streamlit.script_run_context import get_script_run_ctx
 except ModuleNotFoundError:
     # streamlit < 1.4
-    from streamlit.report_thread import (
-        get_report_ctx as get_script_run_ctx,
-    )
+    from streamlit.report_thread import get_report_ctx as get_script_run_ctx
 
 from streamlit.server.server import Server
 
