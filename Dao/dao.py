fc8afb2f49c41877fcf5f47448ec58dda3690c9e
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2023-07-03 21:21:47 -0400

    Update dao.py

--- Dao/dao.py
+++ Dao/dao.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#import os
-import sys
-#owd=sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
-
-import streamlit as st
-import base64
-
 __author__ = "Supreme ciento"
 __copyright__ = "Copyright 2023, Cloud Bots™ BlackBots"
 __credits__ = ["Supreme Ciento"]
@@ -19,8 +12,16 @@ __status__ = "Production"
 __version__ = "12.21.21"
 
 
+import sys
+import streamlit as st
+import base64
+from bs4 import BeautifulSoup
+import textwrap
+from io import StringIO,BytesIO
+from gtts import gTTS
+import requests
 from streamlit_extras.switch_page_button import switch_page
-
+from streamlit_extras.customize_running import center_running
 
 hide_streamlit_style = """
             <style>
@@ -34,7 +35,48 @@ res_box=st.empty()
 URL = st.text_input(':orange[Enter DaoTranslate.com URL]',key='input',help='Enter your DAOTranslate URL into the input field.')
 
 ok=st.button('📩',help='📖Read',key='1237');memory=[];res_box.markdown(f":blue[Book:  ]")
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+def ReadIt():
+    center_running()
+    url = URL
+    try:
+        res_box.markdown(f":blue[Book:  ]Starting..")
+        resp=requests.get(url)
+    except:
+        res_box.markdown(f":blue[Book:  ]Enter a valid url before running.")
+        raise SystemExit
+    
+    if resp.status_code==200:
+        soup=BeautifulSoup(resp.text,'html.parser')    
+        d=soup.find("div",{"class":"epcontent entry-content"})
+        st.write("# Auto-playing Audio!")
+        speech=BytesIO()
+        speech_=gTTS(text=d.text,lang='en',slow=False)
+        #speech_.write_to_fp(speech)
+        speech_.save("dao.mp3")
+        autoplay_audio("dao.mp3")
+    else:
+        res_box.markdown(f":blue[Book: ]There appears to be something wrong with the website.")
+        raise SystemExit
 
+    def Next():
+        ## EDIT #############################
+        oldurl = url
+        chap = ''.join([n for n in oldurl if n.isdigit()])
+        nxtchap = str(int(chap) + int(+1))
+        nxtUrl = str(oldurl.replace(chap, nxtchap))
 if ok:
-    switch_page("trans")
+    ReadIt()
     
