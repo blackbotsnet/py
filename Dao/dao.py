6ddec226b1a57817a3d8baa59be8a5e2790affc8
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2023-07-03 20:19:43 -0400

    Update dao.py

--- Dao/dao.py
+++ Dao/dao.py
@@ -74,7 +74,7 @@ def ReadIt():
                     res_box.markdown(f":blue[Book:  ]"+d.text)
                     st.write("# Auto-playing Audio!")
                     speech=BytesIO();speech_=gTTS(text=d.text,lang='en',slow=False);speech_.write_to_fp(speech)
-                    autoplay_audio(speech_=gTTS(text=d.text,lang='en',slow=False))
+                    autoplay_audio(gTTS(text=d.text,lang='en',slow=False))
                     break
     else:
         res_box.markdown(f":blue[Book: ]There appears to be something wrong with the website.")
