1c8774e502a0b4bc2d66148b65171d4f9df61d7a
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2024-04-12 00:59:35 -0400

    Update pubdao.py

--- pubdao.py
+++ pubdao.py
@@ -130,6 +130,7 @@ for img in image_elements:
     
     with open(f"image_{image_elements.index(img)}.png", "wb") as file:
         file.write(image_data)
+     st.download_button(image_data)
 components.html(f'<img src="{image_data}" width=100 height=100 />', width=200, height=200)
 
 options = Options()
