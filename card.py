fcb4ec8a6edd88bc51eb1244b447bc4de9f1f226
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2024-04-19 01:25:33 -0400

    Update card.py

--- card.py
+++ card.py
@@ -17,10 +17,8 @@ import uuid
 import html
 import hashlib
 
-# Load the side image
 side_image = Image.open('static/4.png')
 
-# Define the Streamlit sidebar
 with st.sidebar:
     st.image(side_image)
     st.caption("Full Sail Student Card Gen.")
@@ -31,15 +29,12 @@ with st.sidebar:
     img = st.text_input('Background Image', value="https://www.ieabroad.com/wp-content/uploads/Full-Sail-University.png", placeholder='https://www.ieabroad.com/wp-content/uploads/Full-Sail-University.png')
     link = st.text_input('URL when Card is clicked')
 
-# Function to generate a unique key
-def generate_unique_key():
+def unique_key():
     unique_id = str(uuid.uuid4())
     hashed_key = hashlib.sha256(unique_id.encode()).hexdigest()
     return hashed_key
 
-# Function to generate the card and save it as an image
-def generate_and_download_card():
-    # Generate the card
+def generate_card():
     card_content = card(
         title=name,
         text=[f"#{stu}", f"{degree}", f"{memo}"],
@@ -48,19 +43,5 @@ def generate_and_download_card():
         key=generate_unique_key()
     )
     
-    @st.cache_data
-    def convert_df(df):
-        return df.to_csv().encode('utf-8')
-    
-    csv = convert_df(card_content)
-    
-    st.download_button(
-        label="Download data as CSV",
-        data=csv,
-        file_name='large_df.csv',
-        mime='text/csv',
-    )
-
-# Generate the card
-generate_and_download_card()
+generate_card()
 
