f522082252ffc4da0337ad6f85b8bf4972da0ebd
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2024-04-19 00:11:13 -0400

    Update card.py

--- card.py
+++ card.py
@@ -1,11 +1,21 @@
 import streamlit as st
 from streamlit_card import card
-name = st.text_input('Your name')
-stu = st.text_input('Student id #')
-degree = st.text_input('Your degree')
-memo = st.text_input('Memo')
-img = st.text_input('Background Image', placeholder='https://www.ieabroad.com/wp-content/uploads/Full-Sail-University.png')
-link = st.text_input('Link')
+
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+with st.sidebar:
+    name = st.text_input('Your name')
+    stu = st.text_input('Student id #')
+    degree = st.text_input('Your degree')
+    memo = st.text_input('Memo')
+    img = st.text_input('Background Image', placeholder='https://www.ieabroad.com/wp-content/uploads/Full-Sail-University.png')
+    link = st.text_input('Link')
+
 def example():
     card(
         title=name,
