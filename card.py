f5076e4dc849ba40fbd4514552bdcd08d3b26537
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2024-04-19 00:15:16 -0400

    Update card.py

--- card.py
+++ card.py
@@ -9,11 +9,11 @@ hide_github_icon = """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 with st.sidebar:
-    name = st.text_input('Your name')
-    stu = st.text_input('Student id #')
-    degree = st.text_input('Your degree')
-    memo = st.text_input('Memo')
-    img = st.text_input('Background Image', placeholder='https://www.ieabroad.com/wp-content/uploads/Full-Sail-University.png')
+    name = st.text_input('Your name', value="@Supreme.Ciento")
+    stu = st.text_input('Student id #', value="012345678")
+    degree = st.text_input('Your degree', value="Game Development")
+    memo = st.text_input('Memo', value="Good luck!")
+    img = st.text_input('Background Image', value="https://www.ieabroad.com/wp-content/uploads/Full-Sail-University.png", placeholder='https://www.ieabroad.com/wp-content/uploads/Full-Sail-University.png')
     link = st.text_input('Link')
 
 def example():
