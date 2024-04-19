92ac572f4bd11f942c87944b9cbb3805ac0c42f4
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2024-04-19 00:25:07 -0400

    Update card.py

--- card.py
+++ card.py
@@ -9,13 +9,19 @@ from streamlit_card import card
 
 st.markdown("""
 <style>
-#GithubIcon {
-  visibility: hidden;
-}
+  #GithubIcon{visibility: hidden;}
+  .styles_terminalButton__JBj5T{visibility: hidden;}
+  .styles_terminalButton__JBj5T{display:none}
+  .viewerBadge_container__r5tak.styles_viewerBadge__CvC9N{visibility: hidden;}
+  .viewerBadge_container__r5tak.styles_viewerBadge__CvC9N{display:none}
 </style>
 """, unsafe_allow_html=True)
+side_image = Image.open('static/4.png')
+
 
 with st.sidebar:
+    st.image(side_image)
+    st.caption(Full Sail Student Card Gen.")
     name = st.text_input('Your name', value="@Supreme.Ciento")
     stu = st.text_input('Student id #', value="012345678")
     degree = st.text_input('Your degree', value="Game Development")
