0f53c621d25d3890af2dc24090353dfa3f56c7f5
blackbotsnet <138516948+blackbotsnet@users.noreply.github.com>
2024-04-19 00:37:47 -0400

    Update card.py

--- card.py
+++ card.py
@@ -2,6 +2,8 @@ import streamlit as st
 from streamlit_card import card
 from PIL import Image
 import io
+import uuid
+import hashlib
 
 # ┌──────────────────────────────────┐
 # │ Copyright © 2024 BlackBots.net   │
@@ -31,12 +33,17 @@ with st.sidebar:
     img = st.text_input('Background Image', value="https://www.ieabroad.com/wp-content/uploads/Full-Sail-University.png", placeholder='https://www.ieabroad.com/wp-content/uploads/Full-Sail-University.png')
     link = st.text_input('URL when clicked')
 
+def generate_unique_key():
+    unique_id = str(uuid.uuid4())
+    hashed_key = hashlib.sha256(unique_id.encode()).hexdigest()
+    return hashed_key
 def generate_card():
     card(
         title=name,
         text=[f"#{stu}", f"{degree}", f"{memo}"],
         image=img,
         url=link,
+        key=generate_unique_key()
     )
 
 # Generate the card
