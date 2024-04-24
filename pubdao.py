bd3b26414b82d475bfa7b9771a2b6215a15d219f
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-24 19:26:59 -0400

    Update pubdao.py

--- pubdao.py
+++ pubdao.py
@@ -659,3 +659,4 @@ if ok:
  
 st.markdown("<br><hr><center>© Cloud Bots™ BlackBots. All rights reserved.  <a href='mailto:admin@blackbots.net?subject=MangaDojutsu!&body=Please specify the issue you are facing with the app.'><strong>BlackBots.net</strong></a></center><hr>", unsafe_allow_html=True)
 st.markdown("<style> footer {visibility: hidden;} </style>", unsafe_allow_html=True)
+ 
